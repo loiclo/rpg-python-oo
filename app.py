8a6badf75fb631e2e6ebc60645096f06c4e170b0
loiclo <lecinloic07@gmail.com>
2024-09-12 14:50:58 +0200

    drake and hero class can fight

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ class RPG_GAME:
         damage_reduction = 1 - (other.defense_points * 0.003) # 1 point de défense annule 0,3% de dégâts
         damage = max(0, self.attack_points * damage_reduction) # Appliquer la réduction sur les dégâts
         other.health -= damage # Appliquer la réduction sur les dégâts
-        print(f"{self.name} attacked {other.name} for {damage} damages !")
-        print(f"{other.name} has {other.health} health points left !")
+        print(f"{self.name} attacked {other.name} for {damage:.0f} damages !")
+        print(f"{other.name} has {other.health:.0f} health points left !")
         
     def defense(self, other):
         damage = max(0, other.attack - self.defense_points)
@@ -23,10 +23,36 @@ class RPG_GAME:
     def damage(self, other):
         damage = self.attack_points - other.defense
 
+class Dragon(RPG_GAME):
+    def __init__(self, name, health, attack_points, defense_points):
+        super().__init__(name, health, attack_points, defense_points)
+        self.health = 100
+        self.attack_points = 10
+        self.defense_points = 5
+        
+        
+class Hero(RPG_GAME):
+    def __init__(self, name, health, attack_points, defense_points):
+        super().__init__(name, health, attack_points, defense_points)
+        self.health = 100
+        self.attack_points = 15
+        self.defense_points = 5
+        
+class Fight():
+    def __init__(self, dragon, hero):
+        self.dragon = dragon
+        self.hero = hero
+        
+    def fight(self):
+        while self.dragon.health > 0 and self.hero.health > 0:
+            self.dragon.attack(self.hero)
+            self.hero.attack(self.dragon)
+        if self.dragon.health <= 0:
+            print("Le dragon est mort !")
+        elif self.hero.health <= 0:
+            print("Le héros est mort !")
 
-
-hero = RPG_GAME("Hero", 100, 20, 5)  # 5 points de défense
-evil = RPG_GAME("Evil", 100, 15, 8)  # 8 points de défense
-
-hero.attack(evil)  # Le héros attaque le méchant
-evil.attack(hero)  # Le méchant attaque le héros
+dragon = Dragon("Dragon", 100, 10, 5)
+hero = Hero("Héros", 100, 15, 5)
+fight = Fight(dragon, hero)
+fight.fight()
